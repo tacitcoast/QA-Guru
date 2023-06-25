dc588ec2c1d500a7e6f61cbbd6db1ac1691ecbbd
Anna Malinovskaia <tacitcoast@gmail.com>
2023-06-25 20:24:45 +0100

    Update test_csv.py

--- Python_part_3_files/tests/test_csv.py
+++ Python_part_3_files/tests/test_csv.py
@@ -1,14 +1,15 @@
 import csv
 import os.path
+from .conftest import path_res
 
 
 def test_csv():
-    with open(os.path.abspath('../resources/username.csv'), 'w') as csvfile:
+    with open(os.path.join(path_res, 'username.csv'), 'w') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
         csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
         csvwriter.writerow(['Alex', 'Serj', 'Yana'])
 
-    with open(os.path.abspath('../resources/username.csv')) as csvfile:
+    with open(os.path.join(path_res, 'username.csv')) as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
             print(row)
