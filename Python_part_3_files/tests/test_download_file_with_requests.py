72b685b79d0c292361af606cff8653090f790c1a
Anna Malinovskaia <tacitcoast@gmail.com>
2023-06-25 20:25:28 +0100

    Update test_download_file_with_requests.py

--- Python_part_3_files/tests/test_download_file_with_requests.py
+++ Python_part_3_files/tests/test_download_file_with_requests.py
@@ -1,14 +1,15 @@
 import os.path
 import requests
+from .conftest import path_tmp
 
 
 def test_download_png():
     url = 'https://selenium.dev/images/selenium_logo_square_green.png'
 
     response = requests.get(url)
-    with open(os.path.abspath('../tmp/selenium_logo.png'), 'wb') as file:
+    with open(os.path.join(path_tmp, 'selenium_logo.png'), 'wb') as file:
         file.write(response.content)
 
-    size = os.path.getsize('selenium_logo.png')
+    size = os.path.getsize(os.path.join(path_tmp, 'selenium_logo.png'))
 
     assert 30803 == size
