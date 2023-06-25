452155bbc613d4313c4f1f45145dfebf22e4c46f
Anna Malinovskaia <tacitcoast@gmail.com>
2023-06-25 20:26:09 +0100

    Update test_xlsx.py

--- Python_part_3_files/tests/test_xlsx.py
+++ Python_part_3_files/tests/test_xlsx.py
@@ -1,9 +1,10 @@
 import os
 from openpyxl import load_workbook
+from .conftest import path_res
 
 
 def test_xlsx():
-    workbook = load_workbook(os.path.abspath('../resources/file_example_XLSX_50.xlsx'))
+    workbook = load_workbook(os.path.join(path_res, 'file_example_XLSX_50.xlsx'))
     sheet = workbook.active
     value = sheet.cell(row=3, column=2).value
     print(value)
