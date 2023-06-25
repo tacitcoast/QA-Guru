dfd66d120f2643ccb4c675cd931b534f535afb73
Anna Malinovskaia <tacitcoast@gmail.com>
2023-06-25 20:26:29 +0100

    Update test_zip.py

--- Python_part_3_files/tests/test_zip.py
+++ Python_part_3_files/tests/test_zip.py
@@ -1,14 +1,13 @@
 import os
 import zipfile
 from zipfile import ZipFile
+from .conftest import path_tmp, path_res
 
 
 def test_create_and_add_archive():
-    path_tmp = os.path.dirname(os.path.abspath(__file__)) + '/../tmp'
-    path_res = os.path.dirname(os.path.abspath(__file__)) + '/../resources'
     file_dir = os.listdir(path_res)
 
-    with zipfile.ZipFile(path_tmp + '/test.zip', mode='w', \
+    with zipfile.ZipFile(os.path.join(path_tmp, 'test.zip'), mode='w', \
                          compression=zipfile.ZIP_DEFLATED) as zf:
         for file in file_dir:
             add_file = os.path.join(path_res, file)
@@ -16,8 +15,8 @@ def test_create_and_add_archive():
 
 
 def test_read_archive():
-    path_res = os.path.dirname(os.path.abspath(__file__)) + '/../resources'
-    path_tmp = os.path.dirname(os.path.abspath(__file__)) + '/../tmp'
-    with ZipFile(path_tmp + '/test.zip') as zf:
+    with ZipFile(os.path.join(path_tmp, 'test.zip')) as zf:
         filenames = [os.path.basename(file) for file in zf.namelist()]
+        print(filenames)
+        print(os.listdir(path_res))
         assert filenames == os.listdir(path_res)
